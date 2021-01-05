cc9ed4c686826b5b115d82ac0e2fa1cf9df23da8
Ahmed <wolktheline@yahoo.com>
2021-01-05 13:07:53 -0700

    creating the create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create 
+        user = User.create
+    end 
+
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end 
 end
